abd6039188e70d78de3e42038b6e47aa5baf7188
Francisca Javiera Rudolph <javiera.av@ufl.edu>
2024-03-20 09:31:14 -0400

    Update and rename i_was_here to i_was_here.R

---
+++ i_was_here.R
@@ -0,0 +1,22 @@
+###########################################################################################
+# This file is a script for you to open and edit interactively through the GitHub website. '
+#
+# To edit this file, click on the top right editing button (looks like a pencil).
+# This will open the file and let you start writing.
+# Below, add your name, date of edit, and tell about a good resource you would like to share!
+
+
+# Javi Rudolph - March 20, 2024
+# I've recently come across this book and I'm excited to read it: https://r-spatial.org/book/
+
+# Since this is an R script, I have used `#` before each text line to save as comments. 
+# Here goes and R dataset I enjoy using for examples: https://allisonhorst.github.io/palmerpenguins/
+
+install.packages("palmerpenguins")
+library(palmerpenguins)
+data(package = 'palmerpenguins')
+
+# Your turn!
+#############################################################################################
+
+
